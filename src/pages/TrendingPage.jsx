1f4f34353f78c7d1a179c21b58ae733f27fc88d9
Bima Bimo <satriaabimanyu595@gmail.com>
2024-06-05 19:54:53 +0700

    fix: fixing bug, page doesnt want to show

--- src/pages/TrendingPage.jsx
+++ src/pages/TrendingPage.jsx
@@ -3,73 +3,17 @@ import { Container, Grid, Typography, Box, Button } from '@mui/material';
 import TrendItem from '../components/Trending/TrendItem';
 
 const eventTrends = [
-  {
-    id: 1,
-    title: 'Sedekah Untuk Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Sedekah',
-    target: '250/280',
-    percentage: '85%',
-    image: './src/assets/post.jpg',
-  },
-  {
-    id: 2,
-    title: 'Sedekah Untuk Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Sedekah',
-    target: '100/180',
-    percentage: '74%',
-    image: './src/assets/post.jpg',
-  },
-  {
-    id: 3,
-    title: 'Sedekah Untuk Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Sedekah',
-    target: '250/280',
-    percentage: '85%',
-    image: './src/assets/post.jpg',
-  },
-  {
-    id: 4,
-    title: 'Sedekah Untuk Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Sedekah',
-    target: '100/180',
-    percentage: '74%',
-    image: './src/assets/post.jpg',
-  },
-  {
-    id: 5,
-    title: 'Sedekah Untuk Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Sedekah',
-    target: '250/280',
-    percentage: '85%',
-    image: './src/assets/post.jpg',
-  },
-  {
-    id: 6,
-    title: 'Sedekah Untuk Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Sedekah',
-    target: '100/180',
-    percentage: '74%',
-    image: './src/assets/post.jpg',
-  },
+  { id: 1, title: "Sedekah Untuk Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Sedekah", target: "250/280", percentage: "85%", image: './src/assets/post.jpg' },
+  { id: 2, title: "Sedekah Untuk Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Sedekah", target: "100/180", percentage: "74%", image: './src/assets/post.jpg' },
+  { id: 3, title: "Sedekah Untuk Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Sedekah", target: "250/280", percentage: "85%", image: './src/assets/post.jpg' },
+  { id: 4, title: "Sedekah Untuk Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Sedekah", target: "100/180", percentage: "74%", image: './src/assets/post.jpg' },
+  { id: 5, title: "Sedekah Untuk Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Sedekah", target: "250/280", percentage: "85%", image: './src/assets/post.jpg' },
+  { id: 6, title: "Sedekah Untuk Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Sedekah", target: "100/180", percentage: "74%", image: './src/assets/post.jpg' },
 ];
 
 const volunteerTrends = [
-  {
-    id: 3,
-    title: 'Bantu Saudara Kita Terdampak Krisis Pangan',
-    organization: 'Dompet Peduli',
-    target: '300/350',
-    percentage: '86%',
-    image: './src/assets/post.jpg',
-  },
-  {
-    id: 4,
-    title: 'Bantu Kegiatan Sosial Untuk Pendidikan',
-    organization: 'Dompet Peduli',
-    target: '150/200',
-    percentage: '75%',
-    image: './src/assets/post.jpg',
-  },
+  { id: 3, title: "Bantu Saudara Kita Terdampak Krisis Pangan", organization: "Dompet Peduli", target: '300/350', percentage: '86%', image: './src/assets/post.jpg'},
+  { id: 4, title: "Bantu Kegiatan Sosial Untuk Pendidikan", organization: "Dompet Peduli", target: '150/200', percentage: '75%', image: './src/assets/post.jpg'},
 ];
 
 const FilterButtons = ({ activeFilter, setActiveFilter }) => (
@@ -91,11 +35,6 @@ const FilterButtons = ({ activeFilter, setActiveFilter }) => (
   </Box>
 );
 
-FilterButtons.propTypes = {
-  activeFilter: PropTypes.string.isRequired,
-  setActiveFilter: PropTypes.func.isRequired,
-};
-
 const TrendPage = () => {
   const [activeFilter, setActiveFilter] = useState('event');
   const trends = activeFilter === 'event' ? eventTrends : volunteerTrends;
@@ -103,12 +42,8 @@ const TrendPage = () => {
   return (
     <Container>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 4 }}>
-        <Typography variant="h4" component="h1" fontWeight="bold">
-          Trends
-        </Typography>
-        <Typography variant="body2" component="a" href="#">
-          Lihat Semua
-        </Typography>
+        <Typography variant="h4" component="h1" fontWeight="bold">Trends</Typography>
+        <Typography variant="body2" component="a" href="#">Lihat Semua</Typography>
       </Box>
       <FilterButtons activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
       <Grid container spacing={3}>
